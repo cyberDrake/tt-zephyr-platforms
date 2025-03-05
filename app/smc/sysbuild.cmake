a3962f850e328e90ee35defc85816fe8dcee3349
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-03-04 19:44:01 -0500

    app: smc: generate firmware bundle when running with sysbuild
    Generate firmware bundle when running with sysbuild. Write the bundle to
    update.fwbundle
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- app/smc/sysbuild.cmake
+++ app/smc/sysbuild.cmake
@@ -17,3 +17,40 @@ ExternalZephyrProject_Add(
 # Flash the BMFW application first. That way we can hotload the CMFW
 # application without writing to SPI.
 sysbuild_add_dependencies(FLASH ${DEFAULT_IMAGE} bmc)
+
+if(BOARD STREQUAL "tt_blackhole")
+  # Map board revision names to folder names for spirom config data
+  string(TOUPPER ${BOARD_REVISION} BASE_NAME)
+  set(PROD_NAME "${BASE_NAME}-1")
+  set(CFG_NAME "${BOARD_REVISION}-bootfs")
+elseif(BOARD STREQUAL "native_sim")
+  # Use P100 data files to stand in
+  set(PROD_NAME "P100-1")
+  set(CFG_NAME "p100-bootfs")
+else()
+  message(FATAL_ERROR "No support for board ${BOARD}")
+endif()
+
+set(OUTPUT_BOOTFS ${CMAKE_BINARY_DIR}/tt_boot_fs.bin)
+set(OUTPUT_FWBUNDLE ${CMAKE_BINARY_DIR}/update.fwbundle)
+
+# Generate filesystem
+add_custom_command(OUTPUT ${OUTPUT_BOOTFS}
+  COMMAND ${PYTHON_EXECUTABLE}
+  ${APP_DIR}/../../scripts/tt_boot_fs.py mkfs
+  ${BOARD_DIRECTORIES}/bootfs/${CFG_NAME}.yaml
+  ${OUTPUT_BOOTFS}
+  --build-dir ${CMAKE_BINARY_DIR}
+  DEPENDS ${DEFAULT_IMAGE} bmc)
+
+# Generate firmware bundle that can be used to flash this build on a board
+# using tt-flash
+add_custom_command(OUTPUT ${OUTPUT_FWBUNDLE}
+  COMMAND ${PYTHON_EXECUTABLE}
+  ${APP_DIR}/../../scripts/tt_boot_fs.py fwbundle
+  ${OUTPUT_BOOTFS}
+  ${PROD_NAME}
+  ${OUTPUT_FWBUNDLE}
+  DEPENDS ${OUTPUT_BOOTFS})
+
+add_custom_target(gen_bootfs ALL DEPENDS ${OUTPUT_FWBUNDLE})
